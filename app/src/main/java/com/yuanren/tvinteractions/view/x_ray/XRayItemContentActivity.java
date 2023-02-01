3957bbf212aa4511aa87aa30587b70ec59dbd436
EowynREN <eowynren@163.com>
2023-02-01 01:40:29 -0800

    make back button and movie title bigger

---
+++ app/src/main/java/com/yuanren/tvinteractions/view/x_ray/XRayItemContentActivity.java
@@ -0,0 +1,16 @@
+package com.yuanren.tvinteractions.view.x_ray;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class XRayItemContentActivity extends Activity {
+    public static final String SELECTED_MOVIE_ID = "selectionMovieId";
+    public static final String SELECTED_XRAY_ITEM_ID = "selectionXRayItemId";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
